9e1daf682dfa54e857076480d1c14665d8ca6447
Aron Buzinkay <aron.buzinkay@gmail.com>
2021-06-22 02:47:05 +0200

    Added favicon

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -3,27 +3,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
 	render() {
 		return (
-			<Html lang='no'>
+			<Html lang='en'>
 				<Head>
 					<meta
 						name='description'
-						content='Lær hvordan man lager ekte italiensk pizza eller bestill et pizzaevent hvor vi baker pizza ved ditt event for ditt event!'
-					/>
-					<meta
-						name='twitter:card'
-						content='Pizzakurs, pizzaevent og Nettbuttikk'
-					/>
-					<meta
-						name='twitter:title'
-						content='Francesco Solimeo Pizzachef'
-					/>
-					<meta
-						name='twitter:description'
-						content='Lær hvordan man lager ekte italiensk pizza eller bestill et pizzaevent hvor vi baker pizza ved ditt event for ditt event!'
-					/>
-					<meta
-						name='twitter:image'
-						content='https://where-your-image-is-hosted/name.jpg'
+						content='Lagoon'
 					/>
 				</Head>
 				<body>
