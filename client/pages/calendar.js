67d5fc86361b963ea1616cd7288e5ab740e91860
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-06-22 22:13:31 +0200

    added next and prev date to calendar

--- client/pages/calendar.js
+++ client/pages/calendar.js
@@ -1,6 +1,7 @@
 import { getData } from '../utils/fetchData';
 import { useEffect, useState } from 'react';
 import styles from '../styles/calendar/Calendar.module.scss';
+import {ChevronLeft, ChevronRight} from 'react-feather'
 
 import moment from 'moment';
 import Lesson from '../components/calendar/Lesson';
@@ -9,20 +10,20 @@ const Calendar = () => {
 	const [lessons, setLessons] = useState();
 	const [timetable, setTimetable] = useState({});
 	const [exam, setExam] = useState(false);
+	const [date, setDate] = useState(moment());
 
 	useEffect(async () => {
 		const res = await getData('calendar');
 		setLessons(res);
-	}, []);
+	}, [date]);
 
-  let days= []
+	let days = [];
 
-  for(let i=1; i <= 5; i++){
-    days.push(moment().day(i).format('dd-DD'))
-  }
-
-
-	console.log(days)
+	useEffect(() => {
+		for (let i = 1; i <= 5; i++) {
+			days.push(moment(date).day(i).format('ddd-DD'));
+		}
+	});
 
 	const timeToPosition = (time) => {
 		switch (time) {
@@ -45,11 +46,16 @@ const Calendar = () => {
 	};
 
 	useEffect(() => {
-    let tempTimetable = {};
-	  if (lessons !== undefined) {
-      lessons.sort((a, b) => {return parseInt(a.startTime.split(":")[0]) - parseInt(b.startTime.split(":")[0])})
-      days.map((day, index) => {
-        tempTimetable[day] = [];
+		let tempTimetable = {};
+		if (lessons !== undefined) {
+			lessons.sort((a, b) => {
+				return (
+					parseInt(a.startTime.split(':')[0]) -
+					parseInt(b.startTime.split(':')[0])
+				);
+			});
+			days.map((day, index) => {
+				tempTimetable[day] = [];
 				lessons.map((lesson) => {
 					if (lesson.day !== index + 1) {
 						return;
@@ -57,7 +63,7 @@ const Calendar = () => {
 
 					const { course, startTime } = lesson;
 
-					const lessonDate = moment()
+					const lessonDate = moment(date)
 						.day(lesson.day)
 						.format('YYYY-MM-DD');
 
@@ -65,18 +71,21 @@ const Calendar = () => {
 						lessonDate + ' ' + lesson.endTime
 					);
 
-
-					if(moment().diff(lessonDateTime) > 0) {
+					if (moment().diff(lessonDateTime) > 0) {
 						tempTimetable[day].push({
 							...lesson,
-							day: moment().day(lesson.day).format('YYYY-MM-DD'),
+							day: moment(date)
+								.day(lesson.day)
+								.format('YYYY-MM-DD'),
 							course: { ...course, color: 'black' },
 							position: timeToPosition(startTime),
 						});
 					} else {
 						tempTimetable[day].push({
 							...lesson,
-							day: moment().day(lesson.day).format('YYYY-MM-DD'),
+							day: moment(date)
+								.day(lesson.day)
+								.format('YYYY-MM-DD'),
 							position: timeToPosition(startTime),
 						});
 					}
@@ -86,9 +95,27 @@ const Calendar = () => {
 		}
 	}, [lessons]);
 
+	const nextDate = () => {
+		setDate(moment(date).add(7, 'days').day(1));
+	};
+
+	const previousDate = () => {
+		setDate(moment(date).add(-7, 'days').day(1));
+	};
+
 	return (
 		<>
 			<div className={styles.container}>
+				<div className={styles.selector}>
+					<div className={styles.dateSelect}>
+						<ChevronLeft onClick={previousDate}/>
+						<p>
+							{moment(date).day(1).format('MMMM DD ')}-
+							{moment(date).day(5).format(' DD, YYYY')}
+						</p>
+						<ChevronRight onClick={nextDate}/>
+					</div>
+				</div>
 				<div className={styles.calendarCard}>
 					<div className={styles.timeCol}>
 						<div className={styles.one}>
