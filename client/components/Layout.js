da09dd66ca960d3387ba2fa2f8ca86fdcced9aa0
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-07-07 01:30:08 +0200

    added whack redirect if not logged in

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { DataContext } from '../store/GlobalState';
 import NavBar from './navbar/NavBar';
 import CalendarDetail from './calendar/Detail';
 import CalendarAdd from './calendar/AddEvent';
+import { useRouter } from 'next/router';
 
 const Layout = ({ children }) => {
+  const { state, dispatch } = useContext(DataContext);
+  const { auth } = state;
+  const router = useRouter()
+
+  useEffect(() => {
+		if (Object.keys(auth).length === 0) router.push('/login');
+	}, [auth]);
+  
   return (
     <div>
-      <CalendarAdd />
-      <CalendarDetail />
-      <NavBar />
-      {children}
+      {Object.keys(auth).length === 0 ? (
+        <div>
+        { children }
+        </div>
+      ) : (
+        <>
+          <CalendarAdd />
+          <CalendarDetail />
+          <NavBar />
+          {children}
+        </>
+      )}
     </div>
   );
 };
